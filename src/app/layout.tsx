9850cc40fb12709c0940c570802abc19e3f72890
IsacSmile <94504780+IsacSmile@users.noreply.github.com>
2026-09-19 21:40:38 +0530

    feat: add Microsoft Clarity script to layout head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,6 +92,18 @@ export default async function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "yktp18a9bf");
+            `,
+          }}
+        />
       </head>
       <body className="bg-[#050608] text-white flex flex-col min-h-screen antialiased">
         <EnquiryModalProvider>
